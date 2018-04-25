fca90a7970323fbf60548ce7cf245df7325c5329
Ben Newman <ben@apollographql.com>
2018-04-24 20:58:07 -0400

    Avoid needless hasOwnProperty check.

--- packages/apollo-utilities/src/util/maybeDeepFreeze.ts
+++ packages/apollo-utilities/src/util/maybeDeepFreeze.ts
@@ -6,7 +6,6 @@ function deepFreeze(o: any) {
 
   Object.getOwnPropertyNames(o).forEach(function(prop) {
     if (
-      o.hasOwnProperty(prop) &&
       o[prop] !== null &&
       (typeof o[prop] === 'object' || typeof o[prop] === 'function') &&
       !Object.isFrozen(o[prop])
